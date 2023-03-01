0aab425762033a6517011604e0dc9e7059c9e9a7
attahchisom6 <attahchisom6@gmail.com>
2023-03-01 13:54:49 +0100

    going througj atree with inorder scheme

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - function to transverse the left, display it, and then
+ * tranverse the right
+ * @tree: pointer to the root node of the tree
+ * @func: pointer to a function whose parameter is the value contained in the 
+ * node
+ *
+ * Return: void
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
