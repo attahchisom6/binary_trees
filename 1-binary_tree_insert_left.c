c50686dcc1f40e6329ad37ca5a37341f5ac8b7b9
attahchisom1clear <attahchisom1@gmail.com>
2023-03-01 04:27:00 +0100

    inserting at the LHS of a binary tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,8 +21,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	if (parent->left != NULL)
 	{
-		parent->left->parent = new;
 		new->left = parent->left;
+		parent->left->parent = new;
 	}
 	parent->left = new;
 	return (new);
