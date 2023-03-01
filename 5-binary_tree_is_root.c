dd08bea4bb754c984a22d5095ff08628882242fc
attahchisom6 <attahchisom6@gmail.com>
2023-03-01 12:18:51 +0100

    checking if a node is a root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -12,7 +12,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (!node)
 		return (0);
 
-	if ((node->right || node->left) && node->parent == NULL)
+	if ((node->right && node->left) && node->parent == NULL)
 		return (1);
 	return (0);
 }
