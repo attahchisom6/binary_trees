f031211f79928d6be59f90482ab0af8754ce7c10
attahchisom6 <attahchisom6@gmail.com>
2023-03-01 11:03:44 +0100

    deleting binary tree elements

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes all elements in a binary tree
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: void
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *temp;
+
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
