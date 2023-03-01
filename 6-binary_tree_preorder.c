a46232fa65308ecf8b1e3771e659bd99009206d5
attahchisom6 <attahchisom6@gmail.com>
2023-03-01 13:10:05 +0100

    going trough a tree with the preorder scheme

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - display root, then keft and finally right;
+ * @tree:pointer to the root node of the tree to transverse
+ * @func: pointer to function who takes as parameter the value in the node
+ *
+ * Return: void
+ */
+
+void binary_tree_preorder(binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	func(tree->n);
+	binary_tree_print(tree);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
