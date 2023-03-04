02457b842637ec8023ec0dbffa99230876c65693
attahchisom6 <attahchisom6@gmail.com>
2023-03-04 16:48:29 +0100

    Binary search

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -2,31 +2,43 @@
 #include "limits.h"
 
 /**
- * helper - helper func for binary_tree_is_bst
- * @tree: pointer to tree node
- * @min: minimum value of BST tree
- * @max: maximum value of BST tree
- * Return: 1 if tree is a valid BST, and 0 otherwise
+ * bst_analyser - analyzed a binary tree to check if it met
+ * the conditions of a BST (binary search tree)
+ * @min: the lowest value of nodes in a binary tree
+ * @max: the maximum value of nodes contained in a binary tree
+ * @tree: pointer to the root of the tree
+ *
+ * Return: 1 if conditions are met, else 0
  */
-static int helper(const binary_tree_t *tree, int min, int max)
+
+int bst_analyser(const binary_tree_t *tree, int min, int max)
 {
+	int analyser;
+
+	/*an empty tree is also a BST*/
 	if (!tree)
 		return (1);
+
 	if (tree->n < min || tree->n > max)
 		return (0);
-	return (helper(tree->left, min, tree->n - 1) &&
-		helper(tree->right, tree->n + 1, max));
+
+	analyser = bst_analyser(tree->left, min, tree->n - 1)
+		&& bst_analyser(tree->right, tree->n + 1, max);
+
+	return (analyser);
 }
 
-
 /**
- * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
- * @tree: pointer to root of tree
- * Return: 1 if tree is a valid BST, and 0 otherwise
+ * binary_tree_is_bst - checks if a given tree is a bst tree
+ * @tree: pointer to the root of the tree
+ *
+ * Return: 1 if it is a BST, else 0
  */
+
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (helper(tree, INT_MIN, INT_MAX));
+
+	return (bst_analyser(tree, INT_MIN, INT_MAX));
 }
